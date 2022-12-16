fa866ec6ed24c1fe3878400d2275011b629768df
Douglas Useda <douglas.useda2@gmail.com>
2022-12-15 22:42:13 -0300

    Cria a migration de Usuários

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,7 +14,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome_completo');
+            $table->char('cpf', 11);
+            $table->date('nascimento');
+            $table->text('foto_documento');
+            $table->text('foto_usuario')->nullable();
+            $table->char('telefone', 11);
+            $table->integer('tipo_usuario');
+            $table->string('chave_pix')->unique();
+            $table->integer('reputacao')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
