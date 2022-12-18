c7a2de51727344a15f56d58674d82282a944499b
Douglas Useda <douglas.useda2@gmail.com>
2022-12-17 23:37:03 -0300

    Faz a action utilizar a ResourceCollection de Diarista

--- app/Http/Controllers/Diarista/ObtemDiaristasPorCEP.php
+++ app/Http/Controllers/Diarista/ObtemDiaristasPorCEP.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Diarista;
 
+use App\Enums\TipoUsuario;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\DiaristaPublico;
+use App\Http\Resources\DiaristaPublicoCollection;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -12,13 +13,13 @@ class ObtemDiaristasPorCEP extends Controller
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return DiaristaPublico
+     * @param  Request  $request
+     * @return DiaristaPublicoCollection
      */
     public function __invoke(Request $request)
     {
-        $diarista = User::where('tipo_usuario', '=', 2)->first();
+        $diarista = User::where('tipo_usuario', '=', TipoUsuario::DIARISTA->value)->get();
 
-        return new DiaristaPublico($diarista);
+        return new DiaristaPublicoCollection($diarista);
     }
 }
