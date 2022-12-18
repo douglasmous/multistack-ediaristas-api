2de0c5c79f0490c1cce5f93b4cf7ad7a8c722bfb
Douglas Useda <douglas.useda2@gmail.com>
2022-12-17 23:47:03 -0300

    Implementa a migration de cidades

--- database/migrations/2022_12_17_234238_create_cidades_table.php
+++ database/migrations/2022_12_17_234238_create_cidades_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('cidades', function (Blueprint $table) {
             $table->id();
+            $table->integer('codigo_ibge');
+            $table->string('cidade');
+            $table->char('estado', 2);
+
             $table->timestamps();
         });
     }
